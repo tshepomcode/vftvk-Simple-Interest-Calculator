3c5e29073a3eaceecad314cb44482f1a021f6784
tshepo <tshepomcode@gmail.com>
2021-09-12 08:14:40 +0200

    html, css and js updated

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var payment = parseInt(principal) + interest;
+    result = document.getElementById("result");
+    result.innerText = "If you deposit " + principal +",\n"
+                    + "at an interest rate of " + rate + "%.\n"
+                    + "You will receive an amount of "
+                    + payment + ".\n"
+                    + "in the year " + year;
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
