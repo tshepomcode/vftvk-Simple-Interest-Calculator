509f17a8f977493fe0bec93f5116bb8154270d64
tshepo <tshepomcode@gmail.com>
2021-09-17 17:53:00 +0200

    validation added on input

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-function compute()
-{
+function compute() {
+
     p = document.getElementById("principal");
     var principal = document.getElementById("principal").value;
-    if(parseInt(principal) <= 0 || principal == '') {
-        alert("Enter a positive number");
-        p.focus();
-    }
 
-    // Maybe use a while loop to validate the principal?
+    // Check that the input is correct
+    checkValue(p);
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
-    var output = "If you deposit <span class='highlight'>" + principal + '</span>,<br>'
-                + "at an interest rate of <span class='highlight'>" + rate + '%</span>.<br>'
-                + "You will receive an amount of <span class='highlight'>" + interest + '</span><br>'
-                + "in the year <span class='highlight'>" + year + '<span>.';
-    var result = document.getElementById("result");
     
+    // Prepare output string
+    var output = "If you deposit <span class='highlight'>" + principal + '</span>,<br>'
+        + "at an interest rate of <span class='highlight'>" + rate + '%</span>.<br>'
+        + "You will receive an amount of <span class='highlight'>" + interest + '</span><br>'
+        + "in the year <span class='highlight'>" + year + '<span>.';
+    var result = document.getElementById("result");
+
     var p = document.createElement("p");
     p.innerHTML = output;
     result.appendChild(p);
+
+    p.focus();
 }
 
 function updateRate() {
@@ -28,12 +30,16 @@ function updateRate() {
     document.getElementById("rate_val").innerText = rateval;
 }
 
-// function checkValue() {
-//     var input = document.getElementById("principal").value;
-//     var principal = document.getElementById("principal");
-//     if(parseInt(input) <= 0) {
-//         alert("Enter a positive number");
-//         principal.focus();
-//     } 
-// }
-        
+function checkValue(element) {
+    var input = element.value;
+    if (parseInt(input) <= 0 || input == '') {
+        alert("Enter a positive number");
+        element.focus();
+        // location = 'index.html';
+        window.location.reload();
+        element.focus();
+    } else {
+        return;
+    }
+}
+
